419392a2cb7c61254e41a8315596cec239001a6d
Md. Arshad <arshadchowdhury46@gmail.com>
2022-12-22 17:30:13 +0600

    first commit after setting up react and vite

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Let's not do that
       </p>
     </div>
   )
