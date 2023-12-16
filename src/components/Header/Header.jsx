253b408b29cd4eed3a45501725a6e704afcba290
Md. Arshad <arshadchowdhury46@gmail.com>
2023-12-17 00:18:08 +0600

    Changed name to resolve build fail error

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "./Head";
 import Navbar from "./Navbar";
 import Search from "./Search";
-import "./header.css";
+import "./Header.css";
 
 const Header = ({ cartItems }) => {
   return (
